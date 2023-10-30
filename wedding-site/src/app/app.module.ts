a255a24191a53073d9fe12c9be83230d0b08145d
ReeceGibbsGit <reecegibbs93@gmail.com>
2023-10-30 20:54:56 +1300

    Pretty much done with the hero banner component

--- wedding-site/src/app/app.module.ts
+++ wedding-site/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroBannerComponent
   ],
   imports: [
     BrowserModule
---
+++ wedding-site/src/app/components/hero-banner/hero-banner.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-banner',
+  templateUrl: './hero-banner.component.html',
+  styleUrls: ['./hero-banner.component.scss'],
+})
+export class HeroBannerComponent {
+  @Input() title: string | undefined;
+  @Input() imageUrl: string =
+    'https://via.placeholder.com/1920x1080/eee?text=16:9';
+}
