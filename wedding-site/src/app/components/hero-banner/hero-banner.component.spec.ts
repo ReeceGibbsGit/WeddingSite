e4cea79f4bc4954df77f0315f2d416f6ece3227a
ReeceGibbsGit <reecegibbs93@gmail.com>
2023-11-06 19:55:59 +1300

    Setting up jest as the test runner and writing the unit tests for the hero banner component

--- wedding-site/src/app/components/hero-banner/hero-banner.component.spec.ts
+++ wedding-site/src/app/components/hero-banner/hero-banner.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HeroBannerComponent } from './hero-banner.component';
+import { Alignment, HeroBannerComponent } from './hero-banner.component';
 import { assignProps } from 'src/helpers/test-helpers';
 
 interface TestSetup {
@@ -29,7 +29,51 @@ function setup({
 
 describe('HeroBannerComponent', () => {
   it('should create', () => {
-    let { component } = setup();
+    const { component } = setup();
     expect(component).toBeTruthy();
+  });
+
+  it('should set the title when it is defined', () => {
+    const { fixture } = setup({ props: { title: 'Test Title' } });
+    fixture.detectChanges();
+
+    const imageTitleElement: HTMLElement =
+      fixture.nativeElement.querySelector('.image-title');
+
+    expect(imageTitleElement.textContent?.trim()).toBe('Test Title');
+  });
+
+  it('should set the image url when it is defined', () => {
+    const { fixture } = setup({
+      props: { imageUrl: 'https://tincan.unroutable.local' },
+    });
+    fixture.detectChanges();
+
+    const imageElement: HTMLElement = fixture.nativeElement.querySelector(
+      '[data-test-id="hero-banner-image"]'
+    );
+
+    expect(imageElement.getAttribute('src')).toBe(
+      'https://tincan.unroutable.local'
+    );
+  });
+
+  describe('when the titlePosition property is defined', () => {
+    it.each([['left'], ['center'], ['right']])(
+      'sets the %i class in image-title element',
+      (position) => {
+        const { fixture } = setup({
+          props: { titlePosition: position as Alignment },
+        });
+        fixture.detectChanges();
+
+        const imageTitleElement: HTMLElement =
+          fixture.nativeElement.querySelector(
+            '[data-test-id="hero-banner-title"]'
+          );
+
+        expect(imageTitleElement.className).toBe(`image-title ${position}`);
+      }
+    );
   });
 });
