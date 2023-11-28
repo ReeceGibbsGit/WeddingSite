cf00ab42400904c15a205d51126a199287639d61
ReeceGibbsGit <reecegibbs93@gmail.com>
2023-11-28 15:00:32 +1300

    Creating modal component

--- wedding-site/src/app/app.module.ts
+++ wedding-site/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeroBannerComponent } from './components/hero-banner/hero-banner.component';
 import { ButtonComponent } from './components/button/button.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroBannerComponent, ButtonComponent],
+  declarations: [AppComponent, HeroBannerComponent, ButtonComponent, ModalComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
